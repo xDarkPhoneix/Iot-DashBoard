1851437f9b2edc45400470d1f62993f0fc79f73b
Its-Abhishek-Kumar <abhishek32004@gmail.com>
2025-06-10 03:09:34 +0530

    bug

--- IotFrontend/src/components/Dashboard.jsx
+++ IotFrontend/src/components/Dashboard.jsx
@@ -12,36 +12,21 @@ const Dashboard = () => {
   const onlineDevices = devices.filter(d => d.status === 'online').length;
   const totalDevices = devices.length;
   const activeAlerts = alerts.filter(a => !a.acknowledged).length;
-<<<<<<< HEAD
   // const socket = io("http://localhost:8000")
 
-  useEffect(() => {
-
-    console.log(devices);
-    
-    // socket.on("sensor-data", (payload) => {
-    //   console.log("Received:", payload);
-      
-    // });
-
-    // return () => {
-    //   socket.off("sensor-data");
-    // };
-  }, []);
-=======
-  //const socket = io("http://localhost:8000")
-
   // useEffect(() => {
-  //   socket.on("sensor-data", (payload) => {
-  //     console.log("Received:", payload);
-      
-  //   });
 
-  //   return () => {
-  //     socket.off("sensor-data");
-  //   };
+  //   console.log(devices);
+    
+  //   // socket.on("sensor-data", (payload) => {
+  //   //   console.log("Received:", payload);
+      
+  //   // });
+
+  //   // return () => {
+  //   //   socket.off("sensor-data");
+  //   // };
   // }, []);
->>>>>>> 96ac00555950c26afad7b544b45531626e82beb5
 
   return (
     <div className="p-6 space-y-6">
